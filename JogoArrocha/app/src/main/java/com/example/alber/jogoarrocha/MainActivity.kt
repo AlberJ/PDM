10b24bc639d2de7a77c168288235793e9db2543a
Alber Jonathas <alberjonathas@gmail.com>
2017-11-01 10:36:41 -0300

    Jogo do Arrocha Número

---
+++ JogoArrocha/app/src/main/java/com/example/alber/jogoarrocha/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.alber.jogoarrocha
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import java.lang.Math.random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var txt : TextView
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val a: Double = random()
+
+
+
+
+    }
+
+
+}
